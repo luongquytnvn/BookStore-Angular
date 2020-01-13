5241d4f408d78d47d0ae0d0b41b49f02e8f081b7
= <=>
2020-01-13 15:18:16 +0700

    publishing list ts

--- src/app/component/admin/publishing/publishing-list/publishing-list.component.ts
+++ src/app/component/admin/publishing/publishing-list/publishing-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {IPublishing} from '../IPublishing';
+import {PublishingService} from '../publishing.service';
 
 @Component({
   selector: 'app-publishing-list',
@@ -6,11 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./publishing-list.component.css']
 })
 export class PublishingListComponent implements OnInit {
+  publishingList: IPublishing[] = [] ;
+  content: string;
 
-
-  constructor() { }
+  constructor(private publishingService: PublishingService) { }
 
   ngOnInit() {
+    this.publishingService.getPublishingList().subscribe(next => {
+      this.publishingList = next;
+      console.log(this.publishingList);
+    }),
+      // tslint:disable-next-line:no-unused-expression
+      err => {this.content = this.content = JSON.parse(err.error).message; };
   }
 
 }
