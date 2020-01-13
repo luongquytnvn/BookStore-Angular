0131b4af63bbc05e9162d75a1c4f8bc5da1ae74c
Hoang <cct.cry@gmai.com>
2020-01-13 11:21:50 +0700

    success category edit

--- src/app/component/admin/category/category-edit/category-edit.component.ts
+++ src/app/component/admin/category/category-edit/category-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {ICategory} from '../ICategory';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {CategoryService} from '../category.service';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-category-edit',
@@ -6,10 +10,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category-edit.component.css']
 })
 export class CategoryEditComponent implements OnInit {
-
-  constructor() { }
+  category: ICategory;
+  categoryForm: FormGroup;
+  file: File;
+  message = false;
+  constructor(
+    private categoryService: CategoryService,
+    private route: ActivatedRoute,
+    private fb: FormBuilder,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.categoryForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(1)]],
+      book: ['', [Validators.required, Validators.minLength(1)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.categoryForm.valid) {
+      const {value} = this.categoryForm;
+      const data = {
+        ...this.category,
+        ...value
+      };
+      this.categoryService.editCategory(data).subscribe( next => {
+        this.message = true;
+        this.ngOnInit();
+      },
+        error => console.log(error)
+      );
+    }
   }
 
 }
