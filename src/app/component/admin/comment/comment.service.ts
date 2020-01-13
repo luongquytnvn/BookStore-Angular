4d2a607b13d0841170717fe08cdeb0cafb2c4095
Hoang <cct.cry@gmai.com>
2020-01-13 15:12:26 +0700

    success comment create

--- src/app/component/admin/comment/comment.service.ts
+++ src/app/component/admin/comment/comment.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {IComment} from './IComment';
+import {IBook} from '../book/IBook';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentService {
+  private url = 'http://localhost:8080/api/admin/comment';
 
-  constructor() { }
+  constructor( private http: HttpClient) { }
+
+  getCommentList(): Observable<IComment[]> {
+    return this.http.get<IComment[]>(this.url);
+  }
+
+  getComment(id: number): Observable<IComment> {
+    return this.http.get<IComment>(this.url + '/' + id);
+  }
+
+  createComment(comment: IComment): Observable<any> {
+    console.log(comment);
+    return this.http.post(this.url, comment);
+  }
+
+  editComment(comment: IComment): Observable<any> {
+    return this.http.put(this.url + '/' + comment.id, comment);
+  }
+
+  deleteComment(id: number): Observable<any> {
+    return this.http.delete(this.url + '/' + id);
+  }
 }
