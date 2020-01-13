859f38548143b2e0ad07dc43af5f6edc96442f9a
= <=>
2020-01-13 09:34:55 +0700

    author create

--- src/app/component/admin/author/author.service.ts
+++ src/app/component/admin/author/author.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {IAuthor} from './IAuthor';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthorService {
+  private url = 'http://localhost:8080/api/admin/author';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAuthorList(): Observable<IAuthor[]> {
+    return this.http.get<IAuthor[]>(this.url);
+  }
+  getAuthor(id: number): Observable<IAuthor> {
+    return this.http.get<IAuthor>(this.url + '/' + id);
+  }
+  createAuthor(author: IAuthor): Observable<any> {
+    console.log(author);
+    return this.http.post(this.url, author);
+  }
+  editAuthor(author: IAuthor): Observable<any> {
+    return this.http.put(this.url + '/' + author.id, author);
+  }
+  deleteAuthor(id: number): Observable<any> {
+    return this.http.delete(this.url + '/' + id);
+  }
 }
