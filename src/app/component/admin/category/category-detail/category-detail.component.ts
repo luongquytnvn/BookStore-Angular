b35a353c6fa36e580933fa8e3edddd15631725d7
Hoang <cct.cry@gmai.com>
2020-01-13 10:58:31 +0700

    success category detail

--- src/app/component/admin/category/category-detail/category-detail.component.ts
+++ src/app/component/admin/category/category-detail/category-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {ICategory} from '../ICategory';
+import {CategoryService} from '../category.service';
+import {ActivatedRoute, Router} from '@angular/router';
+import {FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-category-detail',
@@ -6,10 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category-detail.component.css']
 })
 export class CategoryDetailComponent implements OnInit {
-
-  constructor() { }
+  category: ICategory;
+  constructor(
+    private categoryService: CategoryService,
+    private route: ActivatedRoute,
+    private fb: FormBuilder,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    const id = +this.route.snapshot.paramMap.get('id');
+    console.log(id);
+    this.categoryService.getCategory(id).subscribe(next => {
+      this.category = next;
+      }, error => {
+      console.log(error);
+      this.category = null;
+      }
+    );
   }
 
 }
