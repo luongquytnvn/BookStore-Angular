8dc7c5fca4f94ca58f189c0a8c9c269fb43f2e48
= <=>
2020-01-13 10:04:26 +0700

    language service

--- src/app/component/admin/language/language.service.ts
+++ src/app/component/admin/language/language.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {ILanguage} from './ILanguage';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LanguageService {
+  private url = 'http://localhost:8080/api/admin/language';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  getLanguageList(): Observable<ILanguage[]> {
+    return this.http.get<ILanguage[]>(this.url);
+  }
+  getLanguage(id: number): Observable<ILanguage> {
+    return this.http.get<ILanguage>(this.url + '/' + id);
+  }
+  createLanguage(language: ILanguage): Observable<any> {
+    console.log(language);
+    return this.http.post(this.url, language);
+  }
+  editLanguage(language: ILanguage): Observable<any> {
+    return this.http.put(this.url + '/' + language.id, language);
+  }
+  deleteLanguage(id: number): Observable<any> {
+    return this.http.delete(this.url + '/' + id);
+  }
 }
