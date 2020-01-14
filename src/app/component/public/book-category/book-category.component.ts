972eefcaa5740dce455d6e0ca1e09b3fc65aa417
luongquytnvn <luongquytnvn@gmail.com>
2020-01-14 15:17:21 +0700

    update

---
+++ src/app/component/public/book-category/book-category.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-book-category',
+  templateUrl: './book-category.component.html',
+  styleUrls: ['./book-category.component.css']
+})
+export class BookCategoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
