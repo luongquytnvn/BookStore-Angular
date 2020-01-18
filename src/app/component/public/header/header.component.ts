1a52a44c79c3b96273bef32fd65090548e72723f
luongquytnvn <luongquytnvn@gmail.com>
2020-01-19 00:24:29 +0700

    update theme

--- src/app/component/public/header/header.component.ts
+++ src/app/component/public/header/header.component.ts
@@ -1,6 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {TokenStorageService} from '../../../user/_services/token-storage.service';
 import {CartComponent} from '../cart/cart.component';
+import {AuthorService} from '../../admin/author/author.service';
+import {CategoryService} from '../../admin/category/category.service';
+import {LanguageService} from '../../admin/language/language.service';
+import {PublishingService} from '../../admin/publishing/publishing.service';
+import {ICategory} from '../../admin/category/ICategory';
+import {ILanguage} from '../../admin/language/ILanguage';
+import {IPublishing} from '../../admin/publishing/IPublishing';
 
 @Component({
   selector: 'app-header',
@@ -15,7 +22,15 @@ export class HeaderComponent implements OnInit {
   showModeratorBoard = false;
   username: string;
   isShow = false;
-  constructor(private tokenStorageService: TokenStorageService) { }
+  categoryList: ICategory[];
+  languageList: ILanguage[];
+  publishingList: IPublishing[];
+
+  constructor(private tokenStorageService: TokenStorageService,
+              private category: CategoryService,
+              private language: LanguageService,
+              private publishing: PublishingService) {
+  }
 
   ngOnInit() {
     this.isLoggedIn = !!this.tokenStorageService.getToken();
@@ -29,9 +44,9 @@ export class HeaderComponent implements OnInit {
 
       this.username = user.username;
     }
-  }
-  setIsShow(isShow: boolean) {
-    this.isShow = isShow;
+    this.category.getCategoryList().subscribe(next => (this.categoryList = next), error => (console.log(error)));
+    this.language.getLanguageList().subscribe(next => (this.languageList = next), error => (console.log(error)));
+    this.publishing.getPublishingList().subscribe(next => (this.publishingList = next), error => (console.log(error)));
   }
 
   logout() {
