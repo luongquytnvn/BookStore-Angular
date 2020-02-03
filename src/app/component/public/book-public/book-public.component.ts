2716da1416c0d1ca21bdd16b0327af40f0cbdd5a
luongquytnvn <luongquytnvn@gmail.com>
2020-02-03 23:48:36 +0700

    add pagination

--- src/app/component/public/book-public/book-public.component.ts
+++ src/app/component/public/book-public/book-public.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {IBook} from '../../admin/book/IBook';
 import {BookService} from '../../admin/book/book.service';
 import {CartComponent} from '../cart/cart.component';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-book-public',
@@ -9,16 +10,22 @@ import {CartComponent} from '../cart/cart.component';
   styleUrls: ['./book-public.component.css']
 })
 export class BookPublicComponent implements OnInit {
-  bookList: IBook[] = [];
+  bookList: IBook[];
   content: string;
-
+  page = 1;
+  pageTotal: number;
   constructor(private bookService: BookService,
               private cart: CartComponent) {
   }
 
   ngOnInit() {
-    this.bookService.getBookList().subscribe(next =>
-      (this.bookList = next), err =>
+    this.bookService.getBookList()
+      // .pipe(map(res => res.filter((book, i) => i < (+this.page * 12) && i >= (+this.page * 12 - 12))))
+      .subscribe(next => {
+        this.bookList = next;
+        this.pageTotal = Math.ceil(+this.bookList.length / 12);
+        console.log(this.pageTotal);
+      }, err =>
       (this.content = this.content = JSON.parse(err.error).message));
   }
 
@@ -27,5 +34,20 @@ export class BookPublicComponent implements OnInit {
       console.log(next);
       this.ngOnInit();
     }, error => console.log(error));
+  }
+
+  changePage(page) {
+    switch (page) {
+      case 'previous':
+        if (this.page > 1) {
+          this.page = this.page - 1;
+        }
+        break;
+      case 'next':
+        if (this.page < this.pageTotal) {
+          this.page = this.page + 1;
+        }
+        break;
+    }
   }
 }
